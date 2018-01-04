c525b0301be44edf1789009566de79df97ce53de
Aurelie Lebec <aurelie.lebec@gmail.com>
2018-01-04 16:59:43 -0600

    [button]update

--- middleware/twilioNotifications.js
+++ middleware/twilioNotifications.js
@@ -4,7 +4,7 @@ function formatMessage(challenger) {
   return `You have been challenged by ${challenger}`;
 }
 
-exports.notifyOnChallenge = (challenger) => {
+exports.notifyOnChallenge = (challenger, phone) => {
   const messageToSend = formatMessage(challenger);
   twilioClient.sendSms('+15043430627', messageToSend);
 };
