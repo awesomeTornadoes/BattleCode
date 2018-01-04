05dac6d89a52e653e939a4e0b63308c9126fe38f
PrestonWinstead <prestonwinstead@me.com>
2018-01-04 15:54:25 -0600

    [challenge] style drawer with challenges

--- public/js/React Components/DashBoard/NavBar.jsx
+++ public/js/React Components/DashBoard/NavBar.jsx
@@ -59,7 +59,7 @@ export default class NavBar extends Component {
           onRequestChange={open => this.setState({ open })}
           open={this.state.open}
         >
-          Your open challenges:
+          <p style={{ marginTop: '10px', marginLeft: '10px' }}>Your open challenges:</p>
           {menuItems}
         </Drawer>
       </div>
