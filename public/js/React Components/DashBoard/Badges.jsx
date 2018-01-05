edd8b4fb7ce6800c4c6bc57fb94ef86b63f1ef5c
PrestonWinstead <prestonwinstead@me.com>
2018-01-05 14:17:08 -0600

    [cleanup] clean errors from badges component

--- public/js/React Components/DashBoard/Badges.jsx
+++ public/js/React Components/DashBoard/Badges.jsx
@@ -36,6 +36,7 @@ export default class Badges extends Component {
   render() {
     const avatar1 = (
       <Avatar
+        key="J"
         color={white}
         backgroundColor={blue300}
         size={50}
@@ -45,6 +46,7 @@ export default class Badges extends Component {
     );
     const avatar2 = (
       <Avatar
+        key="S"
         color={white}
         backgroundColor={red300}
         size={50}
@@ -62,7 +64,7 @@ export default class Badges extends Component {
           <h1> Your Badges </h1>
         </div>
         <h3>Your points : {this.state.badges}</h3>
-        <p>{avatarList}</p>
+        <h5>{avatarList}</h5>
       </div>
     );
   }
